b2045917c761dec79350128fcf5fe3d5afa1b092
Cody O'Brien <65051473+CodySoccer16@users.noreply.github.com>
2024-03-08 09:57:32 -0600

    new premier league

---
+++ PremierLeague/teamData.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/PremierLeague/epl_results_2022-23.csv')
+
+
+clean_df = df[['HomeTeam', 'AwayTeam', 'HomeGoals', 'AwayGoals', 'WinningTeam']]
+print(clean_df.head())
+
+TeamWins = clean_df['WinningTeam'].value_counts()
+#TeamWins = TeamWins["Man City", "Arsenal", "Man United", "Liverpool", "Newcastle", "Tottenham", "Brighton", "Aston Villa", "Fulham", "Brentford", "Crystal Palace", "Chelsea", "Bournemouth", "West Ham", "Wolves", "Nottingham", "Leicester", "Everton", "Leeds", "Southampton"]
+print(dict(TeamWins)) #Fix this to remove the draw function
+"""
+TeamWinPercent = (TeamWins / 38) * 100
+print(TeamWinPercent)
+
+TeamWinPercent.plot(kind='bar', legend=True)
+plt.xlabel('Team')
+plt.ylabel('Win Percentage')
+plt.title('Win Percentage of Premier League Teams')
+plt.show()
+"""
