cbd640dc4f5687fb42bd15df7c344cb334a027c8
cobrien109 <cobrien109@lsr7sd.org>
2024-03-12 08:41:44 -0500

    change to goals

--- PremierLeague/TeamStats.py
+++ PremierLeague/TeamStats.py
@@ -4,19 +4,19 @@ import numpy as np
 
 df = pd.read_csv('data/PremierLeague/squad_pl_stats3.csv')
 df.dropna(inplace=True)
-#print(df)
+df.sort_values(by=["Squad"], inplace=True)
 
-newdf = df[["Squad", "Wins", "Win%", "Poss"]]
+newdf = df[["Squad", "Wins", "Win%", "Poss", "Gls"]]
 print(list(newdf['Squad']))
 
 x = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
-y = np.array(list(df['Win%']))
+y = np.array(list(df['Gls']))
 my_xticks = list(newdf['Squad'])
 plt.xticks = (x, my_xticks)
 plt.plot(x, y)
 plt.xlabel('Team')
-plt.ylabel('Win Percentage')
-plt.title('Win Percentage of Premier League Teams')
+plt.ylabel('Goals Scored')
+plt.title('Goals scored for each Premier League Teams')
 plt.show()
 """
 x = np.array([0,1,2,3])
